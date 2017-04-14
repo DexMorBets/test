b30af92d957f8258890ddbce89967bb99a68345d
DexMor <dexmor93@gmail.com>
2017-04-14 03:28:20 +0300

    First commit

--- testproject/settings.py
+++ testproject/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
