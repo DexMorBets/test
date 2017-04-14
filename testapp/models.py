dfaa9cafc22782951c8aedb3be8a6e4a25373279
DexMor <dexmor93@gmail.com>
2017-04-14 21:13:28 +0300

    asdasdasd

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Detail(models.Model):
+    class Meta:
+        verbose_name = 'Деталь'
+        verbose_name_plural = 'Детали'
+
+    title = models.CharField(verbose_name='Название детали', max_length=200, db_index=True)
+    price = models.IntegerField(verbose_name='Закупочная цена')
+    count = models.IntegerField(verbose_name='Количество')
+
+    def __str__(self):
+		return self.title
